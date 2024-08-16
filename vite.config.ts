a547b979cb1135950e3c9288d8d9cb70278d0899
Fouadkanzaoui <140793214+Fouadkanzaoui@users.noreply.github.com>
2024-08-16 09:31:24 +0100

    fix maximumFileSizeToCacheInBytes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { ConfigEnv, defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
@@ -17,35 +16,38 @@ export default ({ mode }: ConfigEnv) => {
           short_name: `JERA ${process.env.VITE_APP_NAME}`,
           theme_color: '#ffffff',
           icons: [
-              {
-                  src: 'pwa-64x64.png',
-                  sizes: '64x64',
-                  type: 'image/png'
-              },
-              {
-                  src: 'pwa-192x192.png',
-                  sizes: '192x192',
-                  type: 'image/png'
-              },
-              {
-                  src: 'pwa-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-                  purpose: 'any'
-              },
-              {
-                  src: 'maskable-icon-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-                  purpose: 'maskable'
-              }
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any'
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            }
           ],
-        }, 
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Increase to 5MB
+        }
       })
     ],
     base: `/EKA2-FE/`,
     server: {
-      port: parseInt(process.env.VITE_PORT||"5173"),
+      port: parseInt(process.env.VITE_PORT || "5173"),
     },
     build: {
       minify: false,
@@ -59,6 +61,5 @@ export default ({ mode }: ConfigEnv) => {
     define: {
       "process.env": process.env,
     },
-  })
-}
- 
+  });
+};
